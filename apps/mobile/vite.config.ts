99573a76f7ef8ad930ab91680d7541ceb37dcc7f
thedinj <thedinj@gmail.com>
2026-04-16 02:46:17 -0500

    Remove @vitejs/plugin-legacy

--- apps/mobile/vite.config.ts
+++ apps/mobile/vite.config.ts
@@ -1,12 +1,11 @@
 /// <reference types="vitest" />
 
-import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), legacy()],
+    plugins: [react()],
     server: {
         port: 8100,
         proxy: {
